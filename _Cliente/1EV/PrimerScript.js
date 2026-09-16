00ffb2ebce5fb609db88d0278c3658cf6a84852a
SamuelCarneroJM <Samuel.carnero.jimenez@iesjulianmarias.es>
2026-09-16 09:20:03 +0200

    UpdateClase

---
+++ _Cliente/1EV/PrimerScript.js
@@ -0,0 +1,17 @@
+console.log("Hola mundo");
+console.log("Hola mundo2");
+console.log("Hola mundo3");
+console.log("Hola mundo4");
+console.log("Hola mundo5");
+
+let x = 5;
+let y = 2;
+
+console.log(x);
+console.log(y);
+
+let resultado = x + y;
+let resultado2 = x / 0;
+
+console.log("Suma:", resultado);
+console.log("División:", resultado2);
