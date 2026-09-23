dfd8af21050ea6acd9d4d3733a4cbb8219ed6f46
SamuelCarneroJM <329713665+SamuelCarneroJM@users.noreply.github.com>
2026-09-23 09:06:53 +0200

    repasoMatrices

--- _Cliente/1EV/Unidad2/EjemplosArrays.js
+++ _Cliente/1EV/Unidad2/EjemplosArrays.js
@@ -14,7 +14,7 @@ lista1[5] = "Pepe";
 //     console.log(lista1[i]);
 // }
 
-lista1["nombre"] = "Laura";
+lista1.nombre = "Laura";
 lista1.pos2 = "Santi"; //usar esta
 let pos3 = 6;
 lista1[pos3] = 7;
@@ -29,19 +29,8 @@ console.log(lista1["nombre"]);
 console.log(lista1["pos2"]);
 console.log(lista1.nombre);
 
-persona = new persona("Santi", 30, "Hombre");
-persona2 = new persona("Laura", 25, "Mujer");
-
-persona.length = 3;
-
-arrayPersona = persona.Array = Array();
-
-for (let i = 0; i < arrayPersona.length; i++) {
-    console.log(arrayPersona[i]);
-}
-
-
-
+// -- MATRICES --
+console.log("-- MATRICES --");
 let lista4 = Array();
 let lista5 = Array();
 
@@ -55,4 +44,30 @@ lista5[0] = "Laura";
 lista5[1] = "Santi";
 lista5[2] = "Pepe";
 
-console.log(lista4[2][1]); 
+console.log(lista4[2][1]);
+
+let tabla1 = Array();
+tabla1[0] = [11, 12];
+tabla1[2] = [21, 22, 23];
+tabla1[3] = [31, 32, 33];
+
+// for (let i = 0; i < tabla1.length; i++) {
+//     if (tabla1[i] != undefined) {
+//         for (let j = 0; j < tabla1[i].length; j++) {
+//             console.log(tabla1[i][j]);
+//         }
+//     }
+// }
+
+
+let tabla3 = [[1, 2, 3], [4, 5, 6]];
+
+tabla3[5] = [7, 8, 9];
+
+for (let i = 0; i < tabla3.length; i++) {
+    if (tabla3[i] != undefined) {
+        for (let j = 0; j < tabla3[i].length; j++) {
+            console.log(tabla3[i][j]);
+        }
+    }
+}
